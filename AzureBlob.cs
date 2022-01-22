efca90016647b985d66bf65994334860d997c2d7
WeAreUSS <Walt@WeAreUSS.com>
2022-01-22 12:00:24 -0500

    Changed Title

---
+++ AzureBlob.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace CloudTutorial
+{
+    public class AzureBlob
+    {
+        private static string storageconnstring = "access key"; //ToDo: figure this out....
+        
+        private static string containerName = "container";  
+        private static string filename = "birthdayphoto.jpg";  
+        private static string filepath = "C:\\Nirankari\\birthdayphoto.jpg";  
+        private static string downloadpath = "C:\\Nirankari\\Azure\\birthdayphoto.jpg";  
+
+        public string GetBlob(string containerName, string fileName)
+        {
+            //ToDo: figure out connection string for this....
+            string connectionString = $"yourconnectionstring";
+ 
+            // Setup the connection to the storage account
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+ 
+            // Connect to the blob storage
+            CloudBlobClient serviceClient = storageAccount.CreateCloudBlobClient();
+            // Connect to the blob container
+            CloudBlobContainer container = serviceClient.GetContainerReference($"{containerName}");
+            // Connect to the blob file
+            CloudBlockBlob blob = container.GetBlockBlobReference($"{fileName}");
+            // Get the blob file as text
+            string contents = blob.DownloadTextAsync().Result;
+ 
+            return contents;
+        }
+
+        public  async Task CreateAndSaveAzureBlob() 
+        {  
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageconnstring);  
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);  
+            BlobClient blobClient = containerClient.GetBlobClient(filename);  
+            using FileStream uploadFileStream = File.OpenRead(filepath);  
+            await blobClient.UploadAsync(uploadFileStream, true);  
+            uploadFileStream.Close();  
+        }
+
+        public async Task GetImageBlob(string downloadpath)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageconnstring);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blob = containerClient.GetBlobClient(filename);
+            BlobDownloadInfo blobdata = await blob.DownloadAsync();
+            using (FileStream downloadFileStream = File.OpenWrite(downloadpath))
+            {
+                await blobdata.Content.CopyToAsync(downloadFileStream);
+                downloadFileStream.Close();
+            }
+        }
+
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,20 +6,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace CloudTutorial.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private AzureBlob MyBlob = new AzureBlob();
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+           
         }
 
-        public void OnGet()
+        public async void OnGet()
         {
+            // ToDo: handle file list of all .jpg files
+             MyBlob.GetBlob("container", "cloudtutorial/cloudImages/DSCN1669.jpg");
 
+            await MyBlob.GetImageBlob( "cloudtutorial/cloudImages/DSCN1669.jpg");
+        }
+
+        public void saveEm()
+        {
+            // ToDo: handle file list of all .jpg files c:\cloudimages\
+            MyBlob.CreateAndSaveAzureBlob().Wait();  
         }
     }
 }
